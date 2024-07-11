a45000b76d3cd3aeac4cf0dbace5d5405eafddac
Riri Oba <oobar0523@gmail.com>
2024-07-11 20:46:07 +0900

    complete header

---
+++ utip/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'UTip App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const UTip(),
+    );
+  }
+}
+
+class UTip extends StatefulWidget {
+  const UTip({super.key});
+
+  @override
+  State<UTip> createState() => _UTipState();
+}
+
+class _UTipState extends State<UTip> {
+  @override
+  Widget build(BuildContext context) {
+    print(context.widget);
+    var theme = Theme.of(context);
+    //Add style
+    final style = theme.textTheme.titleMedium!.copyWith(
+        color: theme.colorScheme.onPrimary, fontWeight: FontWeight.bold);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('UTip'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+              padding: const EdgeInsets.all(18),
+              decoration: BoxDecoration(
+                color: theme.colorScheme.inversePrimary,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    "Total per Person",
+                    style: style,
+                  ),
+                  Text(
+                    "\$23.89",
+                    style: style.copyWith(
+                        color: theme.colorScheme.onPrimary,
+                        fontSize: theme.textTheme.displaySmall?.fontSize),
+                  ),
+                ],
+              )),
+        ],
+      ),
+    );
+  }
+}
